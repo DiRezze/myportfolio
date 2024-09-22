b2879fd0201f60c42c41842592552efcf9a2520c
Gabriel Bortoleti Di Rezze <bortoletigabriel.drz@gmail.com>
2024-09-22 14:16:23 -0300

    fix: stackList code refactor

--- src/components/stackList.jsx
+++ src/components/stackList.jsx
@@ -1,52 +1,47 @@
 import React from 'react';
-import { BiLogoBootstrap, BiLogoCPlusPlus, BiLogoCss3, BiLogoFirebase, BiLogoHtml5, BiLogoJava, BiLogoJavascript, BiLogoPython, BiLogoTypescript } from 'react-icons/bi';
 import { FaReact } from 'react-icons/fa';
 import { RiTailwindCssFill } from 'react-icons/ri';
 import { SiKotlin } from 'react-icons/si';
+import { 
+    BiLogoBootstrap, 
+    BiLogoCPlusPlus, 
+    BiLogoCss3, 
+    BiLogoFirebase, 
+    BiLogoHtml5, 
+    BiLogoJava, 
+    BiLogoJavascript, 
+    BiLogoPython, 
+    BiLogoTypescript 
+} from 'react-icons/bi';
 
-const Icons = ({ list }) => {
-    return (
-        <>
-            {list.map(stack => {
-                switch (stack) {
-                    case "html":
-                        return <BiLogoHtml5 key={stack} size={24} className='stack-ico' />;
-                    case "bootstrap":
-                        return <BiLogoBootstrap key={stack} size={24} className='stack-ico' />;
-                    case "css":
-                        return <BiLogoCss3 key={stack} size={24} className='stack-ico' />;
-                    case "cpp":
-                        return <BiLogoCPlusPlus key={stack} size={24} className='stack-ico' />;
-                    case "kotlin":
-                        return <SiKotlin key={stack} size={24} className='stack-ico' />;
-                    case "react": 
-                        return <FaReact key={stack} size={24} className='stack-ico' />;
-                    case "js":
-                        return <BiLogoJavascript key={stack} size={24} className='stack-ico' />;
-                    case "java":
-                        return <BiLogoJava key={stack} size={24} className='stack-ico' />;
-                    case "firebase":
-                        return <BiLogoFirebase key={stack} size={24} className='stack-ico' />;
-                    case "py":
-                        return <BiLogoPython key={stack} size={24} className='stack-ico' />;
-                    case "ts":
-                        return <BiLogoTypescript key={stack} size={24} className='stack-ico' />;
-                    case "tailwind":
-                        return <RiTailwindCssFill key={stack} size={24} className='stack-ico' />;
-                    default:
-                        return null;
-                }
-            })}
-        </>
-    );
+const iconMap = {
+    html: BiLogoHtml5,
+    bootstrap: BiLogoBootstrap,
+    css: BiLogoCss3,
+    cpp: BiLogoCPlusPlus,
+    kotlin: SiKotlin,
+    react: FaReact,
+    js: BiLogoJavascript,
+    java: BiLogoJava,
+    firebase: BiLogoFirebase,
+    py: BiLogoPython,
+    ts: BiLogoTypescript,
+    tailwind: RiTailwindCssFill,
 };
 
-const StackList = ({thisList}) => {
-    return(
-        <div className='card-stack'>
-            <Icons list={thisList}/>
-        </div>
-    );
-}
+const Icons = ({ list }) => (
+    <>
+        {list.map(stack => {
+            const IconComponent = iconMap[stack];
+            return IconComponent ? <IconComponent key={stack} size={24} className='stack-ico' /> : null;
+        })}
+    </>
+);
+
+const StackList = ({ thisList }) => (
+    <div className='card-stack'>
+        <Icons list={thisList} />
+    </div>
+);
 
 export default StackList;
